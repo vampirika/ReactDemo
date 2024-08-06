30eb19f7250e5b0680a7430b8a7ed7b9b9b770db
B.Towner <59606542+vampirika@users.noreply.github.com>
2024-08-06 15:10:59 +0100

    Added fun css

--- src/pages/Gossip.tsx
+++ src/pages/Gossip.tsx
@@ -1,6 +1,6 @@
 // Homepage.js
 import React from 'react';
-import './homepage.css';
+import './gossip.css';
 import PlaySoundButton from '../components/SoundButton.tsx';
 
 const Gossip = () => {
@@ -8,6 +8,8 @@ const Gossip = () => {
       <div className="homepage-wrapper">
         <div className="homepage-box">
             <p>GOSSIP</p>
+            <img className="fun-image" src="images/gossipguys.png" alt="Gossip Guys"></img>
+
             <PlaySoundButton soundUrl="sounds/loot.mp3"></PlaySoundButton>
         </div>
       </div>
