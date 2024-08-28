88a972853d80f277f347708fcc3940278a61ab5f
B.Towner <59606542+vampirika@users.noreply.github.com>
2024-08-28 15:44:22 +0100

    Turned nav menu into hamburger menu

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,21 +1,35 @@
 // Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isRevealed, setIsRevealed] = useState(false);
+  
+  const handleReveal = () => {
+    setIsRevealed(prevState => !prevState);
+  };
+
   return (
     <header className="header-wrapper">
         <div className="header">
           <span>React Demo</span>
         </div>
 
-        <nav className="nav-menu">
-          <Link to="/" >Homepage</Link>
-          <Link to="/buttons" >Buttons</Link>
-          <Link to="/images" >Images</Link>
-          <Link to="/crud" >CRUD</Link>
-          <Link to="/games" >Games</Link>
+        <nav className="navbar">
+          <div className="burger-menu" id="burger-menu" onClick={handleReveal}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+          </div>
+
+          <ul className={`nav-links ${isRevealed ? 'active' : ''}`}>
+            <li><Link to="/" onClick={handleReveal}>Homepage</Link></li>
+            <li><Link to="/buttons" onClick={handleReveal}>Buttons</Link></li>
+            <li><Link to="/images" onClick={handleReveal}>Images</Link></li>
+            <li><Link to="/crud" onClick={handleReveal}>CRUD</Link></li>
+            <li><Link to="/games" onClick={handleReveal}>Games</Link></li>
+          </ul>
         </nav> 
     </header>
   );
